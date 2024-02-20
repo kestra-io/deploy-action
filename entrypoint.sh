e210a7828f02c8245242fb6560da3d4fabee8545
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-02-20 18:08:00 +0100

    feat(action): add apiToken input
    This commit allows specifying an apiToken param to authenticate to Kestra API (EE only).

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,14 @@ if [[ $9 ]]; then
   tenant="--tenant=$9"
 fi
 
+# Check if there is a API Token
+apiToken=""
+if [[ ${10} ]]; then
+  apiToken="--api-token=${10}"
+fi
+
 if [[ $4 == "namespace_files" ]]; then
-  /app/kestra namespace files update "$1" "$2" "$3" --server="$5" "$auth" $delete $tenant
+  /app/kestra namespace files update "$1" "$2" "$3" --server="$5" "$auth" $delete $tenant $apiToken
 else
-  /app/kestra "$4" namespace update "$1" "$2" --server="$5" "$auth" $delete $tenant
+  /app/kestra "$4" namespace update "$1" "$2" --server="$5" "$auth" $delete $tenant $apiToken
 fi
