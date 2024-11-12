ac82e3965776a1edf8a902ff115f6eae8d922187
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-11-12 08:27:58 +0100

    feat: add usage events
    close kestra-io/kestra-ee#2076

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,26 @@ if [[ ${10} ]]; then
   apiToken="--api-token=${10}"
 fi
 
+# Track usage
+CONFIG=$(curl -s https://api.kestra.io/v1/config)
+PH_TOKEN=$(echo "$CONFIG" | sed -E  's/.*"token":"([^"]+)".*/\1/')
+PH_API_HOST=$(echo "$CONFIG" | sed -E  's/.*"apiHost":"([^"]+)".*/\1/')
+PH_ID=$(echo "$CONFIG" | sed -E  's/.*"id":"([^"]+)".*/\1/')
+
+curl -v -L -H "Content-Type: application/json" -d '{
+    "api_key": "'"$PH_TOKEN"'",
+    "event": "Github Actions",
+    "distinct_id": "'"$PH_ID"'",
+    "properties": {
+        "gha": {
+            "name": "deploy-action",
+            "resource": "'"$4"'",
+            "namespace": "'"$1"'"
+        }
+    }
+}' "$PH_API_HOST"capture/ > /dev/null
+
+# Run it
 if [[ $4 == "namespace_files" ]]; then
   /app/kestra namespace files update "$1" "$2" "$3" --server="$5" $auth $delete $tenant $apiToken
 else
